4cc59ae0b1e64012d0c1e3fc576a21c23a81365c
Terry Lorber <terry@metatooth.com>
2019-01-13 10:59:16 -0500

    Triangle and circle!

--- metalogo.sh
+++ metalogo.sh
@@ -3,77 +3,102 @@
 pi=`echo "4*a(1)" | bc -l`
 rad=`echo "60*($pi/180)" | bc -l`
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-    echo "usage: metalogo.sh <radius> <height>"
+    echo "usage: metalogo.sh <radius>"
     exit
 fi
 
 R=$1
-H=$2
-
-function tan()
-{
-    echo "scale=5;s($1)/c($1)" | bc -l
-}
 
 tan_sixty=`echo "s($rad)/c($rad)" | bc -l`
-twice_tan_sixty=`echo "2*$tan_sixty" | bc -l`
+tan_thirty=`echo "s($rad/2)/c($rad/2)" | bc -l`
+sine_thirty=`echo "s($rad/2)" | bc -l`
 
-b=`echo "$H/$twice_tan_sixty" | bc -l`
-x=`echo "$b*s($rad/2)" | bc -l`
-delta=`echo "($H/2)-$x" | bc -l`
+# Calculate big triangle from R
+
+X=`echo "$R * $sine_thirty" | bc -l`
+B=`echo "$X / $tan_thirty" | bc -l`
+H=`echo "$B * $tan_sixty" | bc -l`
+
+
+# Small triangle is 1/4 height of big triangle
+
+h=`echo "$H / 4" | bc -l`
+
+b=`echo "$h / $tan_sixty" | bc -l`
+x=`echo "$b * $tan_thirty" | bc -l`
+r=`echo "$x / $sine_thirty" | bc -l`
 
 ax=`echo "$R" | bc -l`
-ay=`echo "$R-($H/2)-$delta" | bc -l`
+ay=0
 
-bx=`echo "$R-($b/2)" | bc -l`
-by=`echo "$R-($H/4)-$delta" | bc -l`
+bx=`echo "$R - $b" | bc -l`
+by=`echo "$h" | bc -l`
 
-cx=`echo "$R+($b/2)" | bc -l`
-cy=`echo "$R-($H/4)-$delta" | bc -l`
+cx=`echo "$R + $b" | bc -l`
+cy=`echo "$h" | bc -l`
 
-dx=`echo "$R-$b" | bc -l`
-dy=`echo "$R-$delta" | bc -l`
+dx=`echo "$R - (2 * $b)" | bc -l`
+dy=`echo "2 * $h" | bc -l`
 
 ex=`echo "$R" | bc -l`
-ey=`echo "$R-$delta" | bc -l`
+ey=`echo "2 * $h" | bc -l`
 
-fx=`echo "$R+$b" | bc -l`
-fy=`echo "$R-$delta" | bc -l`
+fx=`echo "$R + (2 * $b)" | bc -l`
+fy=`echo "2 * $h" | bc -l`
 
-gx=`echo "$R-(3*($b/2))" | bc -l`
-gy=`echo "$R+($H/4)-$delta" | bc -l`
+gx=`echo "$R - (3 * $b)" | bc -l`
+gy=`echo "3 * $h" | bc -l`
 
-hx=`echo "$R-($b/2)" | bc -l`
-hy=`echo "$R+($H/4)-$delta" | bc -l`
+hx=`echo "$R - $b" | bc -l`
+hy=`echo "3 * $h" | bc -l`
 
-ix=`echo "$R+($b/2)" | bc -l`
-iy=`echo "$R+($H/4)-$delta" | bc -l`
+ix=`echo "$R + $b" | bc -l`
+iy=`echo "3 * $h" | bc -l`
 
-jx=`echo "$R+(3*($b/2))" | bc -l`
-jy=`echo "$R+($H/4)-$delta" | bc -l`
+jx=`echo "$R + (3 * $b)" | bc -l`
+jy=`echo "3 * $h" | bc -l`
 
-kx=`echo "$R-(2*$b)" | bc -l`
-ky=`echo "$R+($H/2)-$delta" | bc -l`
+kx=`echo "$R - (4 * $b)" | bc -l`
+ky=`echo "4 * $h" | bc -l`
 
-lx=`echo "$R-$b" | bc -l`
-ly=`echo "$R+($H/2)-$delta" | bc -l`
+lx=`echo "$R - (2 * $b)" | bc -l`
+ly=`echo "4 * $h" | bc -l`
 
 mx=`echo "$R" | bc -l`
-my=`echo "$R+($H/2)-$delta" | bc -l`
+my=`echo "4 * $h" | bc -l`
 
-nx=`echo "$R+$b" | bc -l`
-ny=`echo "$R+($H/2)-$delta" | bc -l`
+nx=`echo "$R + (2 * $b)" | bc -l`
+ny=`echo "4 * $h" | bc -l`
 
-ox=`echo "$R+(2*$b)" | bc -l`
-oy=`echo "$R+($H/2)-$delta" | bc -l`
+ox=`echo "$R + (4 * $b)" | bc -l`
+oy=`echo "4 * $h" | bc -l`
 
 height=`echo "2*$R" | bc -l`
 width=`echo "2*$R" | bc -l`
 
 cat << EOF
 <svg height="$height" width="$width">
+  <!--
+  pi is $pi
+  60 deg. is $rad radians
+  tan 60 is $tan_sixty
+  tan 30 is $tan_thirty
+  sine 30 is $sine_thirty
+  
+  Radius is $R
+
+  X is $X
+  B is $B
+  H is $H
+
+  h is $h
+  x is $x
+  b is $b
+  r is $r
+
+  -->
   <circle r="$R" cx="$R" cy="$R" fill="black"/>
   <polygon points="$ax,$ay $ox,$oy $kx,$ky" fill="white"/>
   <polygon points="$dx,$dy $fx,$fy $mx,$my" fill="black"/>
