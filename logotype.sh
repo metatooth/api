7be8aaad5431ac7215a83a28b14968c40a24c145
Terry Lorber <tgl@rideside.net>
2019-05-25 22:35:57 -0400

    Make it

--- logotype.sh
+++ logotype.sh
@@ -11,7 +11,19 @@ fi
 
 color=$1
 
-convert -font GaramondNo8-Regular -pointsize 200 label:Meta meta.gif
-convert -font GaramondNo8-Medium -pointsize 200 label:tooth tooth.gif
-convert +append meta.gif tooth.gif metatooth.gif
-convert metatooth.gif -transparent white metatooth.png
+./metalogo.sh 75 0.95 45 black white > logo.svg
+
+convert logo.svg logo.png
+
+convert -fill $color -stroke $color -font GaramondNo8-Regular -pointsize 200 label:Meta meta.png
+convert -fill $color -stroke $color -font GaramondNo8-Medium -pointsize 200 label:tooth tooth.png
+
+convert -size 50x80 canvas:white spacer.png
+
+convert -size 75x80 canvas:white top.png
+
+convert -append top.png logo.png padded-logo.png
+
+convert +append spacer.png meta.png tooth.png padded-logo.png spacer.png temp.png
+
+convert temp.png -transparent white metatooth.png
