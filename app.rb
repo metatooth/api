1e7e433f6cf7078c705a777ed6bdb15a5c976ce4
Terry Lorber <tgl@rideside.net>
2019-05-31 09:29:58 -0400

    Implement authentication by token.

--- app.rb
+++ app.rb
@@ -28,6 +28,12 @@ class App < Sinatra::Base
   end
 
   before do
+    if params[:token]
+      puts "TOKEN!"
+      user = User.find_by_access_token(params[:token])
+      session[:uid] = user.id if !user.nil?
+    end
+    response['Access-Control-Allow-Origin'] = '*'
     @user = User.get(session[:uid])
   end
 
@@ -35,20 +41,36 @@ class App < Sinatra::Base
     'Hello, anonymous.'
   end
 
-  post '/signin' do
+  options '/v1/signin' do
+    response['Access-Control-Allow-Origin'] = '*'
+    response['Access-Control-Allow-Headers'] = 'Content-Type'
+    response['Access-Control-Allow-Methods'] = 'POST'
+  end
+
+  post '/v1/signin' do
+    response['Access-Control-Allow-Origin'] = '*'
     json = JSON.parse(request.body.read)
     if (user = User.authenticate(json['username'], json['password']))
       session[:uid] = user.id
+      return user.issue_access_token
     else
       halt 401
     end
   end
 
-  get '/signout' do
+  options '/v1/signout' do
+    response['Access-Control-Allow-Methods'] = 'GET'
+  end
+
+  get '/v1/signout' do
     session[:uid] = nil
   end
 
-  post '/signup' do
+  options '/v1/signup' do
+    response['Access-Control-Allow-Methods'] = 'POST'
+  end
+
+  post '/v1/signup' do
     content_type :json
     json = JSON.parse(request.body.read)
     if (user = User.signup(json['username'], json['password']))
@@ -58,8 +80,14 @@ class App < Sinatra::Base
     end
   end
 
-  get '/version' do
+  get '/v1/version' do
     { path: '/v1/meals', version: Version.string }.to_json
+  end
+
+  options '/v1/meals' do
+    response['Access-Control-Allow-Origin'] = '*'
+    response['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
   end
 
   get '/v1/meals', auth: 'user' do
