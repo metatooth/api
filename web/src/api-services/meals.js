f5a9e589737e1dfaeb7b678d0a75e2a6a39423de
Terry Lorber <tgl@rideside.net>
2019-06-03 10:54:48 -0400

    Authentication by URL param

--- web/src/api-services/meals.js
+++ web/src/api-services/meals.js
@@ -8,18 +8,18 @@ export default {
     },
 
     get: function (id, token) {
-        return HTTP.get(`${RESOURCE_NAME}/${id}`, { headers: { Authorization: 'Bearer ' + token } })
+        return HTTP.get(`${RESOURCE_NAME}/${id}`)
     },
 
     create: function (data, token) {
-        return HTTP.post(`${RESOURCE_NAME}?token=${token}`, data, { headers: { Authorization: 'Bearer ' + token } })
+        return HTTP.post(`${RESOURCE_NAME}?token=${token}`, data)
     },
 
     update: function (id, data, token) {
-        return HTTP.put(`${RESOURCE_NAME}/${id}?token=${token}`, data, { headers: { Authorization: 'Bearer ' + token } })
+        return HTTP.put(`${RESOURCE_NAME}/${id}?token=${token}`, data)
     },
 
     destroy: function (id, token) {
-        return HTTP.delete(`${RESOURCE_NAME}/${id}?token=${token}`, { headers: { Authorization: 'Bearer ' + token } })
+        return HTTP.delete(`${RESOURCE_NAME}/${id}?token=${token}`)
     },
 }
