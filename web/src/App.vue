3f02489c5c1b78808c24e33b4ec8f7c963742fa3
Terry Lorber <tgl@rideside.net>
2019-06-04 11:05:22 -0400

    Fixup Editor with record & cache properties.

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,7 +4,7 @@
     <sign-up v-if="signup_visible" :on-close="show_signin"></sign-up>
     <sign-in v-if="signin_visible" :on-signin="do_signin" :on-signup="show_signup"></sign-in>
     <tracker v-if="tracker_visible" :meals="meals" :token="access_token" :on-edit="show_editor"></tracker>
-    <editor v-if="editor_visible" :meal="active_meal" :token="access_token" :on-close="show_tracker"></editor>
+    <editor v-if="editor_visible" :record="active_meal" :cache="cache_meal" :token="access_token" :on-close="show_tracker"></editor>
   </div>
 </template>
 
@@ -30,6 +30,7 @@ export default {
     return {
       access_token: '',
       active_meal: null,
+      cache_meal: null,
       editor_visible: false,
       meals: [],
       signin_visible: true,
@@ -53,9 +54,17 @@ export default {
     },
     do_signout: function() {
       this.access_token = ''
+      this.show_signin()
     },
     show_editor: function (meal) {
       this.active_meal = meal
+      
+      this.cache_meal = {}
+      this.cache_meal['id'] = meal['id']
+      this.cache_meal['taken'] = meal['taken']
+      this.cache_meal['text'] = meal['text']
+      this.cache_meal['calories'] = meal['calories']
+
       this.signin_visible = false
       this.signup_visible = false
       this.tracker_visible = false
--- web/src/components/Editor.vue
+++ web/src/components/Editor.vue
@@ -2,9 +2,33 @@
   <section class="section">
     <div class="field">
         <div class="control">
-            <input type="number" v-model="meal.calories">
+            <label for="taken">Meal At</label>
+            <input type="text" v-model="record.taken">
         </div>
     </div>
+    <div class="field">
+        <div class="control">
+            <label for="text">Description</label>
+            <input type="text" v-model="record.text">
+        </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <label for="calories">Calories</label>
+            <input type="number" v-model="record.calories">
+        </div>
+    </div>
+    <div class="field is-grouped">
+        <p class="control">
+            <a class="button is-primary" :disabled="!isComplete" @click="save()">Save</a>
+        </p>
+        <p class="control">
+            <a class="button is-light" @click="cancel()">Cancel</a>
+        </p>
+    </div>
+    <p class="help is-danger"> 
+        {{ error }}
+    </p>
   </section>
 </template>
 
@@ -13,8 +37,47 @@ import MealsService from '../api-services/meals.js'
 
 export default {
     props: {
-        meal: {
+        cache: {
             type: Object
+        },
+        onClose: {
+            type: Function,
+            default: function () {
+
+            }
+        },
+        record: {
+            type: Object
+        },
+        token: {
+            type: String
+        }
+    },
+    data: function () {
+        return {
+            error: ''
+        }
+    },
+    computed: {
+        isComplete: function () {
+            return (this.record.taken.length > 0 && this.record.text.length > 0 && this.record.calories > 0)
+        }
+    },
+    methods: {
+        cancel: function () {
+            this.record['taken'] = this.cache['taken']
+            this.record['text'] = this.cache['text']
+            this.record['calories'] = this.cache['calories']
+            this.onClose()
+        },
+        save: function () {
+            MealsService.update(this.record['id'], this.record, this.token)
+              .then(response => {
+                this.onClose()
+              }).catch(error => {
+                  console.log(error)
+                  this.error = error
+              })
         }
     }
 }
