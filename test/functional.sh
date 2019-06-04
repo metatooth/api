2d0f7c675449cceb5375bc200117b3fd0032dc2e
Terry Lorber <tgl@rideside.net>
2019-06-04 14:00:46 -0400

    Test by user type.

--- test/functional.sh
+++ test/functional.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne 1 ]; then
     echo "Usage: functional.h [URL]"
     echo "       Run functional tests against a URL"
-    echo "       For example, functional.sh http://localhost:9393/v1"
+    echo "       For example, ./functional.sh http://localhost:9393/v1"
     exit
 fi
 
@@ -21,37 +21,66 @@ function check_response () {
 URL=$1
 
 
-echo "SIGN UP"
-STATUS=$(curl --write-out "%{http_code}\n" -d @tester-signin.json --output response.json -H 'Content-Type: application/json' $URL/signup)
+echo "SIGN UP USER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @user-signin.json --output response.json -H 'Content-Type: application/json' $URL/signup)
+echo $STATUS
+cat response.json
+
+echo "SIGN UP USER MANAGER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @user-manager-signin.json --output response.json -H 'Content-Type: application/json' $URL/signup)
+echo $STATUS
+cat response.json
+
+echo "SIGN UP ADMIN"
+STATUS=$(curl --write-out "%{http_code}\n" -d @admin-signin.json --output response.json -H 'Content-Type: application/json' $URL/signup)
 echo $STATUS
 cat response.json
 
 
-echo "SIGN IN"
-STATUS=$(curl --write-out "%{http_code}\n" -d @tester-signin.json --output response.json -c cookie.txt -H 'Content-Type: application/json' $URL/signin)
+echo "SIGN IN USER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @user-signin.json --output response.json -c user-cookie.txt -H 'Content-Type: application/json' $URL/signin)
 echo $STATUS
 
-echo "GET MEALS"
-STATUS=$(curl --write-out "%{http_code}\n" --silent --output response.json -b cookie.txt -v $URL/meals)
+echo "SIGN IN USER MANAGER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @user-manager-signin.json --output response.json -c user-manager-cookie.txt -H 'Content-Type: application/json' $URL/signin)
+echo $STATUS
+
+echo "SIGN IN ADMIN"
+STATUS=$(curl --write-out "%{http_code}\n" -d @admin-signin.json --output response.json -c admin-cookie.txt -H 'Content-Type: application/json' $URL/signin)
+echo $STATUS
+
+
+echo "GET MEALS as USER"
+STATUS=$(curl --write-out "%{http_code}\n" --silent --output response.json -b user-cookie.txt -v $URL/meals)
 check_response STATUS
 
-echo "POST MEALS"
-STATUS=$(curl --write-out "%{http_code}\n" -d @create.json --output response.json -b cookie.txt -H 'Content-Type: application/json' $URL/meals)
+echo "POST MEALS as USER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @create.json --output response.json -b user-cookie.txt -H 'Content-Type: application/json' $URL/meals)
 check_response STATUS
 
 ID=`jq -s -r .[0].id response.json`
 
-echo "GET MEAL $ID"
-STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b cookie.txt -H 'Content-Type: application/json' -v $URL/meals/$ID)
+echo "GET MEAL $ID as USER"
+STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b user-cookie.txt -H 'Content-Type: application/json' -v $URL/meals/$ID)
 check_response STATUS
 
-echo "PUT MEAL $ID"
-STATUS=$(curl --write-out "%{http_code}\n" -d @update.json --output response.json -b cookie.txt -X PUT -H 'Content-Type: application/json' $URL/meals/$ID)
+echo "PUT MEAL $ID as USER"
+STATUS=$(curl --write-out "%{http_code}\n" -d @update.json --output response.json -b user-cookie.txt -X PUT -H 'Content-Type: application/json' $URL/meals/$ID)
 check_response STATUS
 
-echo "DELETE MEAL $ID"
-STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b cookie.txt -X DELETE -H 'Content-Type: application/json' $URL/meals/$ID)
+echo "DELETE MEAL $ID as USER"
+STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b user-cookie.txt -X DELETE -H 'Content-Type: application/json' $URL/meals/$ID)
 check_response STATUS
 
-rm cookie.txt
+echo "GET USERS as USER"
+STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b user-cookie.txt -H 'Content-Type: application/json' $URL/users)
+check_response STATUS
+
+echo "GET USERS as USER MANAGER"
+STATUS=$(curl --write-out "%{http_code}\n" --output response.json -b user-manager-cookie.txt -H 'Content-Type: application/json' $URL/users)
+check_response STATUS
+
+rm user-cookie.txt
+rm user-manager-cookie.txt
+rm admin-cookie.txt
 rm response.json
