11ca9a6ed467a2165c3881f1960f7d25535d0a60
Terry Lorber <tgl@rideside.net>
2019-07-26 11:49:43 -0400

    Add User to the Tracker model.

--- api/tracker.rb
+++ api/tracker.rb
@@ -5,12 +5,22 @@ require_relative 'model'
 # A model of a time tracker
 class Tracker < Model
   attr_accessor :id
+  attr_accessor :user_id
   attr_accessor :created
   attr_accessor :updated
 
   def self.all
     trackers = []
     trackers_ref = @@firestore.col('trackers')
+    trackers_ref.get do |tracker|
+      trackers << Tracker.new(tracker)
+    end
+    trackers
+  end
+
+  def self.find_by_user(user_id)
+    trackers = []
+    trackers_ref = @@firestore.col('trackers').where('user_id', '==', user_id)
     trackers_ref.get do |tracker|
       trackers << Tracker.new(tracker)
     end
@@ -25,9 +35,10 @@ class Tracker < Model
   end
 
   def create
-    if @id.nil?
+    if @id.nil? && valid?
+      @created = @updated = Time.now
       doc_ref = @@firestore.col('trackers').doc
-      doc_ref.set(created: Time.now, updated: Time.now)
+      doc_ref.set(user_id: @user_id, created: @created, updated: @updated)
       initialize(doc_ref.get)
     end
     true if @id
@@ -40,29 +51,48 @@ class Tracker < Model
 
   def init_from_snap(snap)
     @id = snap.document_id
+    @user_id = snap.get('user_id')
     @created = snap.get('created')
     @updated = snap.get('updated')
   end
 
-  def initialize(params = nil)
+  def init_from_string(str)
+    vars = JSON.parse(str)
+    @user_id = vars['user_id']
+  end
+
+  def init_from_hash(params)
+    @user_id = params[:user_id]
+  end
+
+  def initialize(params)
     if params.class == Google::Cloud::Firestore::DocumentSnapshot
       init_from_snap(params)
+    elsif params.class == String
+      init_from_string(params)
+    elsif params.class == Hash
+      init_from_hash(params)
     end
   end
 
   def to_json(*_args)
     {
-      id: @id, created: @created, updated: @updated
+      id: @id, user_id: @user_id, created: @created, updated: @updated
     }.to_json
   end
 
   def update
-    if @id
+    if @id && valid?
+      @updated = Time.now
       resp = @@firestore.col('trackers').doc(@id).set(
-        created: @created, updated: Time.now
+        user_id: @user_id, created: @created, updated: @updated
       )
     end
     true if resp
+  end
+
+  def valid?
+    !@user_id.nil?
   end
 
 end
